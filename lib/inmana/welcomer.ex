ac0ba663cae268a127123e5dfd8790dc14f8cc26
viniciusventura29 <viniciusventura29@gmail.com>
2022-11-21 11:19:00 -0300

    📦 NEW: Creating Welcomer

---
+++ lib/inmana/welcomer.ex
@@ -0,0 +1,24 @@
+defmodule Inmana.Welcomer do
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
+
+    name
+    |> String.trim()
+    |> String.downcase()
+    |> evaluate(age)
+  end
+
+  defp evaluate("banana", 42) do
+    {:ok, "Voce e especial"}
+  end
+
+
+  defp evaluate(name, age) when age >= 18 do
+    {:ok, "Welcome #{name}"}
+  end
+
+  defp evaluate(name, _age) do
+    {:error, "You shall not pass #{name}"}
+  end
+
+end
